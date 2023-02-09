d277dc434d2463d6304976b6d547d01e0800fcd7
Rintaro <reddtse@protonmail.com>
2023-02-09 22:11:57 +0800

    edits for responsive design

--- README.md
+++ README.md
@@ -1,5 +1,50 @@
-# renato_portfolio
-A personal website hand-crafted from scratch.
+# Personal Website#
+
+ Description
+
+This repository contains the source code for my personal website. The website showcases my projects, skills, and personal information.
+
+
+### Technologies
+
+The website is built using the following technologies:
+
+1. HTML
+2. CSS
+3. JavaScript
+4. jQuery
+
+
+### Files
+
+The repository contains the following files:
+
+`index.html` - The main page of the website
+`css` - Directory containing the stylesheets for the website
+`js` - Directory containing the JavaScript files for the website
+`img` - Directory containing the images used in the website
+
+
+### Attribution
+
+The following resources were used in the creation of this website:
+
+Google Fonts - Font library
+Freepik - 
+    - [fashion-young-koreans] (http://www.freepik.com)
+    - [flat-design-data-logo-template] (http://www.freepik.com)
+    - [flat-geometric-models-background] (http://www.freepik.com)
+    - [hand-drawn-round-buttons-collection-social-networks] (http://www.freepik.com)
+
+
+The SVGs and icons are attributed to the folder name as per Freepik's terms. I claim no ownership of the artworks unless otherwise stated.
+
+
+### License
+
+This repository is licensed under the MIT license.
+
+### Author
 
 The author of this website is RENATO TAN
-twitter.com/renatodelarenta
+<br /> [Twitter](http://www.twitter.com/renatodelarenta)
--- js/main.js
+++ js/main.js
@@ -20,3 +20,69 @@
 //   });
 // });
 
+var ctx = document.getElementById('topoChart').getContext('2d');
+var chart = new Chart(ctx, {
+type: 'line',
+data: {
+labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+datasets: [{
+label: 'Sadness',
+data: [0, 2, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+backgroundColor: 'rgba(59, 55, 191, 0.2)',
+borderColor: '#3B37BF',
+borderWidth: 1,
+lineTension: 0.5,
+fill: false
+},
+{
+label: 'Happiness',
+data: [8, 10, 8, 10, 9, 9, 9, 10, 9, 8, 8, 10],
+backgroundColor: 'rgba(91, 87, 235, 0.2)',
+borderColor: '#FFA99A',
+borderWidth: 1,
+lineTension: 0.5,
+fill: false
+},
+{
+label: 'Focus',
+data: [0, 1, 2, 3, 1, 2, 3, 7, 1, 5, 8, 8.5],
+backgroundColor: 'rgba(1, 204, 155, 0.2)',
+borderColor: '#01CC9B',
+borderWidth: 1,
+lineTension: 0.5,
+fill: false
+},
+{
+label: 'Goals',
+data: [0, 1, 2, 1, 1, 3, 4, 5, 6, 7, 8, 9],
+backgroundColor: 'rgba(1, 204, 155, 0.2)',
+borderColor: '#9D9AFF',
+borderWidth: 1,
+lineTension: 0.5,
+fill: false
+}]
+},
+options: {
+scales: {
+yAxes: [{
+ticks: {
+beginAtZero: true
+}
+}]
+}
+}
+});
+
+// JavaScript for Navigation toggle
+function toggleNav() {
+var navigation = document.getElementById("navigation");
+var menuIcon = document.querySelector(".menu-icon");
+navigation.classList.toggle("responsive");
+if (menuIcon.classList.contains("fa-bars")) {
+menuIcon.classList.remove("fa-bars");
+menuIcon.classList.add("fa-times");
+} else {
+menuIcon.classList.remove("fa-times");
+menuIcon.classList.add("fa-bars");
+}
+}
